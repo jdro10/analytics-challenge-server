f9525bf344c4a23c9282407581c3f0bdd9a239d7
Jorge Oliveira <jorgedroliveira10@gmail.com>
2024-05-12 00:55:50 +0100

    added missing json annotations

--- analytics-challenge/Models/Goal.cs
+++ analytics-challenge/Models/Goal.cs
@@ -1,15 +1,34 @@
-﻿namespace analytics_challenge.Models
+﻿using Newtonsoft.Json;
+
+namespace analytics_challenge.Models
 {
     public class Goal
     {
+        [JsonProperty("name")]
         public string? Name { get; set; }
+
+        [JsonProperty("nominal")]
         public bool Nominal { get; set; }
+
+        [JsonProperty("kind")]
         public string? Kind { get; set; }
+
+        [JsonProperty("return_goal")]
         public float? ReturnGoal { get; set; }
+
+        [JsonProperty("start_quarter")]
         public int? StartQuarter { get; set; }
+
+        [JsonProperty("end_quarter")]
         public int? EndQuarter { get; set; }
+
+        [JsonProperty("periodicity")]
         public int? Periodicity { get; set; }
+
+        [JsonProperty("values")]
         public Dictionary<string, float>? Values { get; set; }
+
+        [JsonProperty("portfolios")]
         public List<int>? Portfolios { get; set; }
     }
 }
--- analytics-challenge/Models/Loan.cs
+++ analytics-challenge/Models/Loan.cs
@@ -1,17 +1,40 @@
-﻿namespace analytics_challenge.Models
+﻿using Newtonsoft.Json;
+
+namespace analytics_challenge.Models
 {
     public class Loan
     {
+        [JsonProperty("name")]
         public string? Name { get; set; }
+
+        [JsonProperty("amount")]
         public float Amount { get; set; }
+
+        [JsonProperty("duration")]
         public int Duration { get; set; }
+
+        [JsonProperty("rate")]
         public float Rate { get; set; }
+
+        [JsonProperty("spread")]
         public float Spread { get; set; }
+
+        [JsonProperty("payment")]
         public float Payment {  get; set; }
+
+        [JsonProperty("collateral_portfolios")]
         public List<int>? CollateralPortefolios {  get; set; }
+
+        [JsonProperty("interest_portfolios")]
         public List<int>? InterestPortefolios { get; set; }
+
+        [JsonProperty("amortization_portfolios")]
         public List<int>? AmortizationPortfolios { get; set; }
+
+        [JsonProperty("amortization_schedule")]
         public string? AmortizationSchedule { get; set; }
+
+        [JsonProperty("fixed_interest_duration")]
         public int FixedInterestDuration { get; set; }
     }
 }
